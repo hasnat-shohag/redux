d6f945fffb4ce9228609159e30551054e678cff9
hasnat-shohag <hasnat.ru.ice19@gmail.com>
2024-03-29 17:50:02 +0600

    added redux logger middleware

--- index.js
+++ index.js
@@ -4,9 +4,11 @@
 // store - getState(), dispatch(), subscribe()
 
 // state
-const { createStore } = require("redux");
+const { createStore, applyMiddleware } = require("redux");
+const { default: logger } = require("redux-logger");
 
 const ADD_USER = "addUser";
+const GET_ALL_USER = "getAllUsers";
 
 const initialState = { users: ["Hayat"], count: 1 };
 
@@ -19,26 +21,37 @@ const addUser = (value) => {
 	};
 };
 
+const getAllUsers = () => {
+	return {
+		type: GET_ALL_USER,
+	};
+};
+
 // create a reducer
 const counterReducer = (state = initialState, action) => {
 	switch (action.type) {
 		case ADD_USER:
 			return {
-				...state,
 				users: [...state.users, action.payload],
 				count: state.count + 1,
 			};
+		case GET_ALL_USER:
+			return {
+				users: state.users,
+			};
 		default:
-			state;
+			return state;
 	}
 };
 
 // create a store
-const store = createStore(counterReducer);
+const store = createStore(counterReducer, applyMiddleware(logger));
 
 store.subscribe(() => {
 	console.log(store.getState());
 });
 
 store.dispatch(addUser("Hasnat"));
-store.dispatch(addUser("Sohan"));
+// store.dispatch(addUser("Sohan"));
+
+store.dispatch(getAllUsers());
